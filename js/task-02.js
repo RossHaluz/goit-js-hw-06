5bb19e89eef5ba9ffc2f1e9c1e225bb0666f3fd0
Rostyslav <rossgaluzinskiy@gmail.com>
2022-10-18 23:32:27 +0300

    change

---
+++ js/task-02.js
@@ -0,0 +1,14 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+for (let i = 0; i < ingredients.length; i += 1){
+  const item = document.createElement('li')
+  console.log(item)
+  
+}
