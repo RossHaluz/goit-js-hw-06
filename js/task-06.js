62f523a013ee5b3c5d0c4498d89e11efe334601b
Rostyslav <rossgaluzinskiy@gmail.com>
2022-10-23 22:19:15 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#validation-input')
+const dataInputAtribut = document.querySelector('input[data-length="6"]')
+
+input.addEventListener('blur', auditInput)
+
+function auditInput(even) {
+    if (even.currentTarget.value.length <= dataInputAtribut.dataset.length) {
+        input.classList.add('invalid');
+    } 
+    if (even.currentTarget.value.length > dataInputAtribut.dataset.length) {
+        input.classList.remove('invalid')
+        input.classList.add('valid');
+}
+}
