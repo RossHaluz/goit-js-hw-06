5ebfb557c42d5e32bec4f7805b6770750e2d3f70
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-10-24 11:23:18 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', sendForm);
 
+
+function sendForm(even) {
+    even.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = even.currentTarget;
+    if (email.value === "" || password.value === "") {
+    return alert("Ведіть усі незаповненні поля!");
+    }
+    
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+  even.currentTarget.reset();
+}
