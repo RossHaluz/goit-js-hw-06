8eab1ab0a40476fafa83f560f21a7cae3a1c5514
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-10-24 11:41:32 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const btn = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+btn.addEventListener('click', clickButton);
+
+function clickButton(even) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor()
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
