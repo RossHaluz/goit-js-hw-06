b87a9af73474f6d9898fcbe836ef6e3fbe3f67ce
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-10-25 16:24:11 +0300

    fix task-06

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,11 @@ const dataInputAtribut = document.querySelector('input[data-length="6"]')
 input.addEventListener('blur', auditInput)
 
 function auditInput(even) {
-    if (even.currentTarget.value.length <= dataInputAtribut.dataset.length) {
+    if (even.currentTarget.value.length < dataInputAtribut.dataset.length) {
+        input.classList.remove('valid')
         input.classList.add('invalid');
     } 
-    if (even.currentTarget.value.length > dataInputAtribut.dataset.length) {
+    if (even.currentTarget.value.length >= dataInputAtribut.dataset.length) {
         input.classList.remove('invalid')
         input.classList.add('valid');
 }
