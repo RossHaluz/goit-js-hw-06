3f82839613d631ff19981df028f438806c7c76a3
Rostyslav <rossgaluzinskiy@gmail.com>
2022-10-26 17:31:52 +0300

    fix task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,15 @@
 const input = document.querySelector('#validation-input')
 const dataInputAtribut = document.querySelector('input[data-length="6"]')
+const data = Number(dataInputAtribut.dataset.length)
 
 input.addEventListener('blur', auditInput)
 
 function auditInput(even) {
-    if (even.currentTarget.value.length < dataInputAtribut.dataset.length) {
+    if (even.currentTarget.value.length < data || even.currentTarget.value.length > data) {
         input.classList.remove('valid')
         input.classList.add('invalid');
     } 
-    if (even.currentTarget.value.length >= dataInputAtribut.dataset.length) {
+    if (even.currentTarget.value.length === data) {
         input.classList.remove('invalid')
         input.classList.add('valid');
 }
